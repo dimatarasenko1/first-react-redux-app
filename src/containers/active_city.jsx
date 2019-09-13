4a118830e90f411b3d45900a751d8b10af166667
Dima Tarasenko <taras3nko.dima@gmail.com>
2019-09-13 14:57:36 +0100

    added image display and close active city functionality

--- src/containers/active_city.jsx
+++ src/containers/active_city.jsx
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+
+import { selectCity } from "../actions/index";
 
 class ActiveCity extends Component {
+  handleClick = () => {
+    this.props.selectCity(null);
+  }
+
   render() {
     if (this.props.city) {
+      const link = `https://kitt.lewagon.com/placeholder/cities/${this.props.city.slug}`;
       return (
         <div className="active-city">
           <h3>{this.props.city.name}</h3>
           <h2>{this.props.city.address}</h2>
+          <img src={link} alt=""/>
+          <div>
+            <a href="#" onClick={this.handleClick}>Close</a>
+          </div>
         </div>
       );
     }
     return (
-      <div></div>
+      <div />
     );
   }
 }
@@ -23,4 +35,8 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(ActiveCity);
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ selectCity }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ActiveCity);
